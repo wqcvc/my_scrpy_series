207fc080571c9d3c3d33aae96b38b464d5b67232
wqcvc <273334956@qq.com>
2020-10-14 18:08:16 +0800

    20201014

--- fund/lib_fund.py
+++ fund/lib_fund.py
@@ -24,13 +24,31 @@ class libFund(MyLogger):
         @param level:日志级别
         """
         super().__init__(__name__,level)
-        self.res = libScrpy(level)
+        self.res = libScrpy(level=logging.WARNING)
+        self.fund_list=[]
         if fund_code_list:
             self.fund_list = fund_code_list
         else:  # 读取配置文件的基金列表
-            self.fund_list = 0
+            dict=self.__json_to_dict()
+            for k,v in dict.items():
+                self.fund_list.append(v['code'])
+        self.name, self.jjjz, self.gsz= self.fund_current_jjjz()
 
-    def current_jjjz(self, list_a: list = None):
+
+    def __json_to_dict(self,json_name:str ="fund_list.json"):
+        """
+        将json的格式转换为字典,方便后续处理
+        :param json_name: 默认json文件名
+        :return:
+        """
+        dict=json.load(open(json_name,'r'))
+        if dict:
+            return dict
+        else:
+            self.logger.info("json to dict failed.")
+
+
+    def fund_current_jjjz(self, list_a: list = None):
         """
         基金实时涨跌幅 数据统一获取入口
         @param list_a: 基金代码列表
@@ -40,35 +58,84 @@ class libFund(MyLogger):
             list_a = self.fund_list
         jjjz = []
         name = []
+        gsz = []
         for i in range(len(list_a)):
             self.logger.info(f"request fund_code:[{list_a[i]}]")
             text = self.res.single_request(list_a[i], flag=2,method=0)
-            name_tmp,jjjz_tmp=self.re_current_jjjz(content=text)
-            jjjz.append(jjjz_tmp)
-            name.append(name_tmp)
+            list_b=self.re_current_jjjz(content=text)
+            name.append(list_b[0])
+            jjjz.append(list_b[1])
+            gsz.append(list_b[2])
 
         #展示基金名字+实时估算净值
         for i in range(len(name)):
-            self.logger.info(f"[{name[i]}:{jjjz[i]}]")
+            self.logger.info(f"[{name[i]}]:涨跌幅[{jjjz[i]}]")
 
-    def fund_income_estimate(self,):
+        return name,jjjz,gsz
+
+    def fund_income_estimate(self):
         """
-        根据基金持有份额 估算当前涨跌幅下的当日收益
+        根据基金持有份额num 估算当前涨跌幅下的当日收益
         :return:
         """
-        # json.loads()
-        listA=json.load(open('fund_list.json','r'))
-        self.logger.info(f"listA is : {listA,type(listA)}")
-        self.logger.info(f"listA is : {listA['1']['code']}")
-        for k,v in listA.items():
-            print(k,v,v['code'],v['cost'])
+        dict1 = self.__json_to_dict()
+
+        numbers = []
+        for k, v in dict1.items():
+            numbers.append(v['num'])
+
+        incomes = []
+        for i in range(len(self.name)):
+            incomes.append(float(self.jjjz[i]) * numbers[i])
+            self.logger.info(f"[{self.name[i]}]:涨跌幅[{self.jjjz[i]}]:估算收益[{incomes[i]:.2f}]")
+
+        return incomes
+
 
     def fund_rate_estimate(self):
         """
         根据cost成本估算当前净值下的持有历史总收益率
         :return:
         """
-        pass
+        dict1 = self.__json_to_dict()
+
+        costs = []
+        for k, v in dict1.items():
+            costs.append(v['cost'])
+
+        rates=[]
+        for i in range(len(self.name)):
+            rate=(float(self.gsz[i])/costs[i]-1)*100
+            rates.append(rate)
+            self.logger.info(f"[{self.name[i]}]:持有收益率[{rates[i]:.2f}%]")
+
+        return rates
+
+
+    def fund_hold_amount_income(self):
+        """
+        估算当前净值下的持有总金额及总收益金额
+        :return:
+        """
+        dict1 = self.__json_to_dict()
+
+        costs = []
+        numbers = []
+        for k, v in dict1.items():
+            costs.append(v['cost'])
+            numbers.append(v['num'])
+
+        total_amount = []
+        income_amount = []
+        for i in range(len(self.name)):
+            amount=(float(self.gsz[i]))*numbers[i]
+            income=(float(self.gsz[i])/costs[i]-1)*numbers[i]
+            total_amount.append(amount)
+            income_amount.append(income)
+            self.logger.info(f"[{self.name[i]}]:持有收益[{income_amount[i]:.2f}]:持有总金额[{total_amount[i]:.2f}]")
+
+        return total_amount,income_amount
+
 
     def fund_history_jjjz(self, day: int = 3):
         """
@@ -89,14 +156,20 @@ class libFund(MyLogger):
                 'gz_gsz':'<span class="ui-font-large  ui-num" id="gz_gsz">(.*?)</span>',
                 'name':'<div class="fundDetail-tit"><div style="float: left">(.*?)<span>',
                 'gsname':'"name":"(.*?)"',
-                'gszzl':'"gszzl":"(.*?)"'
+                'gszzl':'"gszzl":"(.*?)"',
+                'gsz':'"gsz":"(.*?)"'
                   }
         # res1=re.findall(re_rules['gz_gsz'],str(content))
         # res2=re.findall(re_rules['name'],str(content))
         name=re.findall(re_rules['gsname'],str(content))
         gszzl=re.findall(re_rules['gszzl'],str(content))
+        gsz=re.findall(re_rules['gsz'],str(content))
 
-        return name[0],gszzl[0]
+        list_t=[]
+        list_t.append(name[0])
+        list_t.append(gszzl[0])
+        list_t.append(gsz[0])
+        return list_t
 
 
     def match_rule_re(self, content):
@@ -142,5 +215,7 @@ class libFund(MyLogger):
 if __name__ == "__main__":
     fund_code_list = ['512000','270002'] #,'000478','110035','001210','008488','001938','002621']
     ff = libFund(fund_code_list,level=logging.INFO)
-    ff.current_jjjz()
+    # ff.fund_current_jjjz()
     ff.fund_income_estimate()
+    ff.fund_rate_estimate()
+    ff.fund_hold_amount_income()
