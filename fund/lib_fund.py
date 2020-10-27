e0baf8266a75d3a1135c11737a89664be7b95206
wqcvc <273334956@qq.com>
2020-10-27 18:48:12 +0800

    xxx

--- fund/lib_fund.py
+++ fund/lib_fund.py
@@ -15,6 +15,47 @@ import logging
 import json
 from lxml import etree
 import csv
+import time
+import functools
+
+
+# 装饰器:执行时间统计
+def timer(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        func(*args, **kwargs)
+        t2 = time.time()
+        print(f"函数名:[{func.__name__}]执行耗时:[{t2 - t1}] seconds.")
+
+    return wrapper
+
+
+# 装饰器: delay 延迟执行
+def delay(sec):
+    def wrapper(func):
+        @functools.wraps(func)
+        def _delay_wrapper(self, *args, **kwargs):
+            time.sleep(sec)
+            print(f"延迟执行,函数名:[{func.__name__}],参数:[args={args}, kwargs={kwargs}]  延迟:[{sec}]s...")
+            func(self, *args, **kwargs)
+
+        return _delay_wrapper
+
+    return wrapper
+
+
+# 装饰器: retry重试函数
+def retry(count, sec):
+    assert count and sec,"轮询次数(count)以及轮询间隔(sec)必须大于0"
+    def wrapper(func):
+        @functools.wraps(func)
+        def _retry_wrapper(*args, **kwargs):
+
+            func(*args, **kwargs)
+
+        return _retry_wrapper()
+
+    return wrapper
 
 
 class libFund(MyLogger):
@@ -35,7 +76,7 @@ class libFund(MyLogger):
             dict = self.__json_to_dict()
             for k, v in dict.items():
                 self.fund_list.append(v['code'])
-        #名称 基金净值 基金
+        # 名称 基金净值 基金
         self.name, self.jjjz, self.gsz = self.fund_current_jjjz()
 
     def __json_to_dict(self, json_name: str = "fund_list.json"):
@@ -125,7 +166,8 @@ class libFund(MyLogger):
             income_amount.append(t_income)
             curr_amount.append(curr_income)
 
-            self.logger.info(f"[{self.name[i]}]:当日收益估算[{curr_amount[i]:.2f}]:持有总收益[{income_amount[i]:.2f}]:持有总金额[{total_amount[i]:.2f}]")
+            self.logger.info(
+                f"[{self.name[i]}]:当日收益估算[{curr_amount[i]:.2f}]:持有总收益[{income_amount[i]:.2f}]:持有总金额[{total_amount[i]:.2f}]")
 
         return total_amount, income_amount
 
@@ -141,32 +183,34 @@ class libFund(MyLogger):
         page = day // 49 + 1  # 要请求的页数
         hisjz_list = []
         for p in range(page):
-            content = self.scrpy.single_request(code=code, flag=3, day=49, page=p+1)  # day=49固定,分页最大49
-            jz_data=self.__re_history_jjjz(content)
+            content = self.scrpy.single_request(code=code, flag=3, day=49, page=p + 1)  # day=49固定,分页最大49
+            jz_data = self.__re_history_jjjz(content)
             # self.logger.info(jz_data)
             for i in range(len(jz_data)):
                 hisjz_list.append(jz_data[i])
-        hisjz_list=hisjz_list[:day]
+        hisjz_list = hisjz_list[:day]
         # self.logger.info(len(hisjz_list))
         # self.logger.info(f"净值日期	单位净值	累计净值	日增长率")
         # for i in range(len(hisjz_list)):
         #     self.logger.info(hisjz_list[i])
-        #day天的总收益率
-        rates_in_day=0
-        for i in  range(len(hisjz_list)):
+        # day天的总收益率
+        rates_in_day = 0
+        for i in range(len(hisjz_list)):
             rates_in_day += float(hisjz_list[i][3])
         # self.logger.info(f"\n该基金最近 [{day}] 天总收益率为:[ {rates_in_day:.2f} ]\n")
 
         return hisjz_list
 
-    def fund_hold_shares(self,code: str):
+    @delay(2)
+    # @timer
+    def fund_hold_shares(self, code: str):
         """
         单个基金持仓股票及其实时涨跌幅
         数据来源url:http://fundf10.eastmoney.com/ccmx_512000.html
         :return: 基金的前10持仓股票的基本信息列表
         """
         assert code, "基金代码必传"
-        content = self.scrpy.single_request(code=code,method=1,flag=4)
+        content = self.scrpy.single_request(code=code, method=1, flag=4)
         quote_info_list = self.__re_quote_hold(content)
         code_name = self.__code_to_name(code)
         # code = self.__name_to_code(name='国投瑞银新兴产业混合')
@@ -174,7 +218,7 @@ class libFund(MyLogger):
         # self.logger.info(quote_info)
         return quote_info_list
 
-    def __code_to_name(self,code: str):
+    def __code_to_name(self, code: str):
         """
         根绝code得到基金名称
         :param code:
@@ -182,10 +226,10 @@ class libFund(MyLogger):
         """
         resp = self.fund_all_funds()
 
-        re_rule={
-            1:"\"xxxxxx\",\".*?\",\"(.*?)\",",
+        re_rule = {
+            1: "\"xxxxxx\",\".*?\",\"(.*?)\",",
         }
-        re_res = re.findall(re_rule[1].replace('xxxxxx',code), str(resp))
+        re_res = re.findall(re_rule[1].replace('xxxxxx', code), str(resp))
         # self.logger.info(f"re_res is : {re_res[0]}")
         if re_res[0]:
             code_name = re_res[0]
@@ -233,11 +277,11 @@ class libFund(MyLogger):
         :param day: 需要获取的天数
         :return:
         """
-        re_rules={
-            "1":"<tr><td>(.*?)</td><td class='tor bold'>(.*?)</td><td class='tor bold'>(.*?)</td><td class='tor bold .*?'>(.*?)%</td><td>.*?</td><td>.*?</td><td class='red unbold'></td></tr>",
-            "2":"<tr>(.*?)</tr>"
+        re_rules = {
+            "1": "<tr><td>(.*?)</td><td class='tor bold'>(.*?)</td><td class='tor bold'>(.*?)</td><td class='tor bold .*?'>(.*?)%</td><td>.*?</td><td>.*?</td><td class='red unbold'></td></tr>",
+            "2": "<tr>(.*?)</tr>"
         }
-        resp=re.findall(re_rules["1"],str(content),re.S|re.M)
+        resp = re.findall(re_rules["1"], str(content), re.S | re.M)
         # self.logger.info(len(resp))
         # for i in range(len(resp)):
         #     self.logger.info(resp[i])
@@ -250,30 +294,30 @@ class libFund(MyLogger):
         :param content: 网页内容，需要解析的
         :return:
         """
-        html=etree.HTML(content)
-        #html = etree.parse(content, etree.HTMLParser()) #文件
-        xpath_rules={
-            1:'//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[1]/text()',  # 序号
-            2:'//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[2]/a/text()',  # 股票代码
-            3:'//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[3]/a/text()',  # 股票名称
-            4:'//*[@id="dq600030"]/text()',  # 最新价 //*[@id="dq600030"]
-            5:'//*[@id="zd600030"]/text()',  # 涨跌幅
-            6:'//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[7]/text()',  # 持仓占比
-            7:'//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[8]/text()',  # 持仓股数万
-            8:'//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[9]/text()'  # 持仓市值
+        html = etree.HTML(content)
+        # html = etree.parse(content, etree.HTMLParser()) #文件
+        xpath_rules = {
+            1: '//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[1]/text()',  # 序号
+            2: '//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[2]/a/text()',  # 股票代码
+            3: '//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[3]/a/text()',  # 股票名称
+            4: '//*[@id="dq600030"]/text()',  # 最新价 //*[@id="dq600030"]
+            5: '//*[@id="zd600030"]/text()',  # 涨跌幅
+            6: '//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[7]/text()',  # 持仓占比
+            7: '//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[8]/text()',  # 持仓股数万
+            8: '//*[@id="cctable"]/div[1]/div/table/tbody/tr[1]/td[9]/text()'  # 持仓市值
         }
 
         # @herf : 链接 text() 文本
-        listA=[]
+        listA = []
         for k in range(10):
-            listB=[]
+            listB = []
             for i in range(8):
-                if i+1 == 4 :
-                    res = html.xpath(xpath_rules[i+1].replace('dq600030',f"dq{listB[1]}"))
-                elif  i+1 == 5:
-                    res = html.xpath(xpath_rules[i+1].replace('zd600030',f"zd{listB[1]}"))
+                if i + 1 == 4:
+                    res = html.xpath(xpath_rules[i + 1].replace('dq600030', f"dq{listB[1]}"))
+                elif i + 1 == 5:
+                    res = html.xpath(xpath_rules[i + 1].replace('zd600030', f"zd{listB[1]}"))
                 else:
-                    res = html.xpath(xpath_rules[i+1].replace('tr[1]',f"tr[{k+1}]"))
+                    res = html.xpath(xpath_rules[i + 1].replace('tr[1]', f"tr[{k + 1}]"))
                 # self.logger.info(f"type(res) res res[0] is: {type(res),res,res[0]}")
                 listB.append(res[0])
             # self.logger.info(f"第[{k+1}]个 current listB result: {listB}")
@@ -306,8 +350,8 @@ class libFund(MyLogger):
         :return:
         """
         self.logger.info(f"csv_file:{csv_name} listA:{listA}")
-        with open(csv_name,'w',encoding='utf-8-sig') as f:
-            writer = csv.writer(f,dialect='excel')
+        with open(csv_name, 'w', encoding='utf-8-sig', newline='') as f:
+            writer = csv.writer(f, dialect='excel')
             writer.writerow(title)
             for row in listA:
                 writer.writerow(row)
@@ -320,7 +364,7 @@ class libFund(MyLogger):
         :param csv_name:
         :return:
         """
-        listA=[]
+        listA = []
         return listA
 
     def db_save(self):
@@ -344,6 +388,7 @@ class libFund(MyLogger):
         :return:
         """
         pass
+
 
 if __name__ == "__main__":
     fund_code_list = ['512000', '270002']  # ,'000478','110035','001210','008488','001938','002621']
